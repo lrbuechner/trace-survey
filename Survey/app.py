81aa0b86bdc4f9b2644c3a72b52645717ce2eec1
Landon Buechner <34969430+lrbuechner@users.noreply.github.com>
2019-11-09 15:09:57 -0600

    Trial Version

--- Survey/app.py
+++ Survey/app.py
@@ -6,7 +6,6 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import dash_table
 import dash_daq as daq
-import dash_dangerously_set_inner_html
 
 import ast
 import numpy as np
@@ -19,22 +18,22 @@ import os
 import psycopg2
 
 # dash plotly 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 # database connection 
 connection = psycopg2.connect(
-            user="---",
-            password="---",
-            host="---",
-            port="---",
-            database="---"
+            user="user",
+            password="password",
+            host="granny",
+            port="able",
+            database="data"
 )
 cursor = connection.cursor()
 
 # querying unique cusips
-query = """SELECT DISTINCT cusip_id FROM live_data """
+query = """SELECT DISTINCT cusip_id FROM trace_lean"""
 unique_cusips = pd.read_sql_query(query, connection)['cusip_id'].values
 # shuffling cusips
 np.random.shuffle(unique_cusips)
@@ -43,7 +42,6 @@ np.random.shuffle(unique_cusips)
 app.layout = html.Div([
 
     html.H1([
-        #html.Img(src=app.get_asset_url('44.png'), style = {'height':'5%', 'width':'5%'}),
         html.Img(src=app.get_asset_url('logo2.png'), style = {'height':'25%', 'width':'25%'}),
 
         ], style={'textAlign':'center',"display": "block","margin-left":"auto","margin-right":"auto",'padding-top': 20}
@@ -116,26 +114,6 @@ app.layout = html.Div([
         ], style = {'textAlign':'left', "display": "block", 'padding-right': 200, 'padding-left': 25, 'border-color': '#6c1420'})
         ]),
         
-        # SURVEY TAB
-        dcc.Tab(id = 'tutorial-tab', label = 'Tutorial', value = 'tutorial-tab', disabled = True, children = [
-
-            html.H6('Selection Options: Clicking or lasso tools. Regardless of selection method, you must hold shift to select more than 1 transaction\nClick "Confirm Selection" to proceed to next bond.',style={'textAlign':'center',"display": "block","margin-left":'5vw',"margin-right":'5vw', 'padding-right': 120}),
-            
-            html.Div([
-                #html.Div([html.Iframe(src="https://www.youtube.com/embed/iWZdz9_Ls_w", width =  1189/2, height = 669/2)]),
-                dash_dangerously_set_inner_html.DangerouslySetInnerHTML('''
-                    <iframe width="1280" height="720" src="https://www.youtube.com/embed/f7Zik6F6uCs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                '''),
-                html.Button('Complete Tutorial', id = 'survey-button', style = {'font-size': '100%'}, n_clicks = 0)
-            ],  style={'textAlign':'center',"display": "block","margin-left":'5vw',"margin-right":'5vw', 'padding-right': 120}),
-
-            html.H6('Note that it takes around 1-5 seconds to query each time series data from our database, this will be optimized before the survey goes live. \
-                Lastly, the above video is a place holder for a quick video tutorial which will \
-                demonstrate how to ensure quality of selection',
-                style={'textAlign':'center',"display": "block","margin-left":'5vw',"margin-right":'5vw', 'padding-right': 120})
-            ]),
-            
-
         # Tab 2 - The Actual Survey, might make this the 3rd tab and add an additional tab that includes a basic tutorial / motivation for this survey
         dcc.Tab(id = 'tab2', label = 'Survey', value ='tab2', disabled=True , children = [
 
@@ -143,13 +121,20 @@ app.layout = html.Div([
                 dcc.Graph(
                     id='indicator-graphic',
                     style={
-                        'height': '35vh',
-                        'width': '65vw',
+                        'height': '45vh',
+                        'width': '55vw',
+                        'max-height': 720,
+                        'min-height': 480,
+                        'max-width': 1280,
+                        'min-width': 720,
                         "display": "block",
                         "margin-left": "auto",
-                        "margin-right": "auto"
+                        "margin-right": "auto",
+                        
                     },
-                    config = {'scrollZoom': True}
+                    config = {'scrollZoom': True, 'showTips': True, 'displaylogo': False, 'doubleClick': 'reset', 
+                    'modeBarButtonsToRemove': ['toImage', 'zoomIn2d', 'zoomOut2d', 'select2d', 'autoScale2d',
+                    'autoScale2d', 'toggleSpikelines', 'hoverClosestCartesian', 'hoverCompareCartesian']}
                 ),
 
                 html.Div([dcc.Checklist(
@@ -159,7 +144,7 @@ app.layout = html.Div([
                         {'label': 'Sell', 'value': 'S'},
                         {'label': 'Inter-Dealer', 'value': 'D'}
                     ],
-                    values=['B', 'S', 'D'],
+                    value=['B', 'S', 'D'],
                     labelStyle={'display': 'inline-block'},
                     style = {
                         'textAlign': 'center',
@@ -187,9 +172,10 @@ app.layout = html.Div([
                 html.Div('Note: You can finish the survey at any point in time!', style={'textAlign':'center',"display": "block","margin-left":"auto","margin-right":"auto"}),
                 html.H3(' '),
 
+                html.Div(id = 'data-table'),
                 html.Div(id='index-log', style = {'display':'none'}),
-                html.Div(id='hidden', style = {'display':'none'}),
-                html.Div(id = 'show-table')   
+                html.Div(id='hidden', style = {'display':'none'})
+                   
                 ], style = { "display": "block", 'padding-right': 120} # or 100, OCD trigger lmao
             )   
         ]),
@@ -249,7 +235,7 @@ def prepare_data(bond):
 
 def order_type_handler(ot):
     # ['b','s']
-    names = ['Buy', 'Sell', 'Inter-Dealer']
+    names = ['Dealer Buy', 'Dealer Sell', 'Inter-Dealer']
     colors = ['rgb(30, 201, 0)', 'rgb(229, 0, 0)','rgb(18, 117, 199)']
 
     nams = [] ; cols = []
@@ -264,15 +250,14 @@ def order_type_handler(ot):
             nams.append(names[2])
             cols.append(colors[2])
     return nams, cols
-
-
     
 # callbacks for scatter plot interaction and data selection 
 @app.callback(
     Output('indicator-graphic', 'figure'),
-    [Input('order-type', 'values'),
+    [Input('order-type', 'value'),
+    Input('indicator-graphic', 'config'),
     Input('confirm-button','n_clicks')])
-def update_graph(order_type, conf_button):
+def update_graph(order_type, config, conf_button):
     
     # control flow for end of survey
     if conf_button < len(unique_cusips):
@@ -280,14 +265,14 @@ def update_graph(order_type, conf_button):
         if conf_button == None:
             conf_button = 0
         
-        # which timeseries/ cusip is currently being loaded
+        # allows for survey taker to loop through.
         click_tracker = conf_button 
         if click_tracker < 0:
             click_tracker = len(unique_cusips) + click_tracker
 
         # querying bond from database hosted on heroku
         cusip_id = unique_cusips[click_tracker]
-        query = """SELECT * FROM live_data WHERE cusip_id = '{}' """.format(cusip_id)
+        query = """SELECT * FROM trace_lean WHERE cusip_id = '{}' """.format(cusip_id)
         bond = pd.read_sql_query(query, connection)
         # making necesary type conversions
         prepare_data(bond)
@@ -296,6 +281,7 @@ def update_graph(order_type, conf_button):
         type_info = order_type_handler(order_type)
         names = type_info[0]
         colors = type_info[1]
+
         return {
             'data': [go.Scattergl(
                     x = bond[bond['rpt_side_cd'] == order_type[i]]['datetime'],
@@ -305,23 +291,22 @@ def update_graph(order_type, conf_button):
                     mode = 'markers',
                     opacity = 1,
                     marker = {
-                        'size': 5,
+                        'size': 7,
                         'color': colors[i],
                         'line': {'width': 0.5, 'color': 'white'}
                         },) for i in range(len(order_type))],
-                'layout': go.Layout(
-                        title='TRACE Outlier Classification | Completed: {}'.format(click_tracker),
-                        margin={'l': 50, 'b': 50, 't': 80, 'r': 80},
-                        yaxis={'title':'Prices'},
-                        legend={'x': 1, 'y': 1},
-                        hovermode='closest',
-                        clickmode = 'event+select'
-                        )
-            }
-    
- 
+            'layout': go.Layout(
+                    title='TRACE Outlier Classification | Completed: {}'.format(click_tracker),
+                    margin={'l': 50, 'b': 50, 't': 80, 'r': 80},
+                    # note the +- 5 is to handle with prices near 0
+                    yaxis={'title':'Prices', 'range': [bond['rptd_pr'].min()*.75-5, bond['rptd_pr'].max()*1.25+5]},
+                    legend={'x': 1, 'y': 1},
+                    hovermode='closest',
+                    clickmode = 'event+select'
+                    )
+        }
+            
 # function that helps convert data from string into python objects. Storing data in HTML div
-
 def default(o):
     if isinstance(o, np.int32): 
         return int(o)  
@@ -331,7 +316,7 @@ def default(o):
 @app.callback(
     [Output('index-log','children'), 
     Output('codex-index','children'),
-    Output('show-table','children')],
+    Output('data-table','children')],
     [Input('indicator-graphic','selectedData'),
     Input('confirm-button','n_clicks'),
     Input('tabs','value')],
@@ -359,7 +344,7 @@ def update_table(selected_data, conf_button, tab, hidden, current, prev_inds):
                 click_tracker = conf_button 
 
                 cusip_id = unique_cusips[click_tracker]
-                query = """SELECT * FROM live_data WHERE cusip_id = '{}' """.format(cusip_id)
+                query = """SELECT * FROM trace_lean WHERE cusip_id = '{}' """.format(cusip_id)
                 bond = pd.read_sql_query(query, connection)
                 # making necesary type conversions
                 prepare_data(bond)
@@ -383,6 +368,7 @@ def update_table(selected_data, conf_button, tab, hidden, current, prev_inds):
                 if max(list(curve_buy['pointNumber'])+[-1]) < len(buy) and \
                     max(list(curve_sell['pointNumber'])+[-1]) < len(sell) and \
                     max(list(curve_D['pointNumber'])+[-1]) < len(D):
+
                     # collecting indicies 
                     b = list(buy.iloc[curve_buy['pointNumber']]['index'])
                     s = list(sell.iloc[curve_sell['pointNumber']]['index'])   
@@ -398,13 +384,16 @@ def update_table(selected_data, conf_button, tab, hidden, current, prev_inds):
                     DT = DT[['Datetime','Price','Volume','B/S/ID']]
 
                     # returning dataTable
-                    return [json.dumps( \
+                    return [
+                        json.dumps( \
                         {'buy': curve_buy['pointNumber'].tolist(), \
                         'sell': curve_sell['pointNumber'].tolist(), \
                         'D': curve_D['pointNumber'].tolist()}, default = default), \
+
                         json.dumps({'inds':indicies}, default = default), \
-                        html.Div([dash_table.DataTable( \
-                            id = 'table', \
+
+                        dash_table.DataTable( \
+                            id = 'a-table', \
                             columns = [{"name": i, "id": i} for i in DT.columns], \
                             data = DT.to_dict("rows"), \
                             style_cell = {'textAlign': 'center','padding': '5px'}, \
@@ -414,11 +403,12 @@ def update_table(selected_data, conf_button, tab, hidden, current, prev_inds):
                                 {'if': {'column_id': 'Price'},'width': '100px'}, \
                                 {'if': {'column_id': 'Volume'},'width': '100px'}], \
                             style_header = {'backgroundColor': 'white', 'fontWeight': 'bold'}, \
-                            n_fixed_rows = 1, \
+                            fixed_rows={'headers': True, 'data': 0 },
+                            page_size= 10,
                             style_table={ \
                                 'height': '20vh', 'width': '50vw', 'maxHeight': '300px', \
                                 'maxWidth': '500px',"margin-left": "auto", "margin-right": "auto"})
-                        ])
+                        
                         ]
                 else:
                     return [json.dumps({'buy': [], 'sell': [], 'D': []}), json.dumps({'inds': PREV}, default=default),'']
@@ -436,8 +426,10 @@ def update_table(selected_data, conf_button, tab, hidden, current, prev_inds):
     Output('hidden','children'),
     Output('dump','children'),
     Output('formatted','children'),
-    Output('order-type','values')],
-    [Input('confirm-button','n_clicks')],
+    Output('order-type','value')],
+    [Input('confirm-button','n_clicks'),
+    Input('finish-surv','submit_n_clicks')
+    ],
     [State('index-log','children'),
     State('hidden','children'),
     State('codex-index','children'),
@@ -447,27 +439,30 @@ def update_table(selected_data, conf_button, tab, hidden, current, prev_inds):
     State('professional-experience','value'),
     State('bond-experience','value')]
 )
-def data_handler(clicks, child, hidden, codex, dump, name, organization, pro_exp, bond_exp):
+def data_handler(clicks, yeet, child, hidden, codex, dump, name, organization, pro_exp, bond_exp):
 
-    if clicks >= 1:
+    if codex == None:
+        codex = json.dumps({'inds': []}, default=default)
+    if dump == None:
+        dump = json.dumps({'inds': []}, default=default)
 
-        new_inds = json.loads(codex)['inds']      
+    new_inds = json.loads(codex)['inds']      
 
-        update_statement = """ 
-            UPDATE survey_data
-            SET indicies = array_cat(indicies, %s) 
-            WHERE username = %s and organization = %s """
-            
-        record_to_update = (str(new_inds).replace('[','{').replace(']','}'), name, organization)
-        cursor.execute(update_statement, record_to_update)
-        connection.commit()
+    update_statement = """ 
+        UPDATE survey_data
+        SET indicies = array_cat(indicies, %s) 
+        WHERE username = %s and organization = %s """
+        
+    record_to_update = (str(new_inds).replace('[','{').replace(']','}'), name, organization)
+    cursor.execute(update_statement, record_to_update)
+    connection.commit()
         
     if codex == None and dump == None:
-        Codex = []
+        _Codex_ = []
     else:
         codex = json.loads(codex)
         dump = json.loads(dump)
-        Codex = list(set(codex['inds'] + dump['inds']))
+        _Codex_ = list(set(codex['inds'] + dump['inds']))
 
     if child != None and hidden != None:
         # merging previously selected to current CUSIP
@@ -482,9 +477,9 @@ def data_handler(clicks, child, hidden, codex, dump, name, organization, pro_exp
         B = B1 + B2
         S = S1 + S2 
         D = D1 + D2
-        return [{"points": []}, json.dumps({'buy': B, 'sell': S, 'D': D}), json.dumps({'inds': Codex}, default=default), '{}', ['B', 'S', 'D']]
+        return [{"points": []}, json.dumps({'buy': B, 'sell': S, 'D': D}), json.dumps({'inds': _Codex_}, default=default), '{}', ['B', 'S', 'D']]
     else:
-        return [{"points": []}, json.dumps({'buy': [], 'sell': [], 'D': []}), json.dumps({'inds': Codex}, default=default), '{}', ['B', 'S', 'D']]
+        return [{"points": []}, json.dumps({'buy': [], 'sell': [], 'D': []}), json.dumps({'inds': _Codex_}, default=default), '{}', ['B', 'S', 'D']]
 
 # user information 
 @app.callback(
@@ -516,16 +511,14 @@ def create_user(clicks, name, organization, pro_exp, bond_exp):
 # Tab transition/toggling 
 @app.callback(
     [Output('tab1','disabled'),
-    Output('tutorial-tab','disabled'),
     Output('tab2','disabled'),
     Output('tab3','disabled'),
     Output('tabs','value')],
     [Input('begin-survey','submit_n_clicks'),
     Input('confirm-button','n_clicks'),
-    Input('survey-button','n_clicks'),
     Input('finish-surv','submit_n_clicks')]
 )
-def next_tab1(begin_clicks, conf_clicks, surv_clicks, finish):
+def next_tab1(begin_clicks, conf_clicks, finish):
 
     # finish survey early
     if finish == 1:
@@ -533,13 +526,11 @@ def next_tab1(begin_clicks, conf_clicks, surv_clicks, finish):
 
     # tab management
     if begin_clicks != 1:
-        return [False,True,True,True,'tab1']
-    elif begin_clicks == 1 and surv_clicks == 0 and conf_clicks != len(unique_cusips):
-        return [True, False, True, True,'tutorial-tab']
-    elif begin_clicks == 1 and surv_clicks == 1 and conf_clicks != len(unique_cusips):
-        return [True, True, False, True,'tab2']
+        return [False,True,True,'tab1']
+    elif begin_clicks == 1 and conf_clicks != len(unique_cusips):
+        return [True, False, True,'tab2']
     elif conf_clicks == len(unique_cusips):
-        return [True, True,True, False,'tab3']
+        return [True,True, False,'tab3']
 
 # sending feedback to database 
 @app.callback(
@@ -554,15 +545,10 @@ def send_feedback(n_clicks, feedback, user, organization):
     if n_clicks == 1:
         
         update_statement = """ UPDATE survey_data SET feedback = %s WHERE username = %s and organization = %s"""
-                
         record_to_update = (feedback, user, organization)
         cursor.execute(update_statement, record_to_update)
         connection.commit()
-
-        survey_exit_html = dash_dangerously_set_inner_html.DangerouslySetInnerHTML(
-            '''<a href="JavaScript:window.close()">Close</a>'''
-            ),
-        return '{}'.format(survey_exit_html)
+        return None
  
 if __name__ == '__main__':
     app.run_server(debug=True)
